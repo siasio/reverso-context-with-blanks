8099bdf5f89955898728f5ffa3fd1cbb5d061b1b
siasio <39811817+siasio@users.noreply.github.com>
2022-03-21 00:39:10 +0100

    from . import

--- __init__.py
+++ __init__.py
@@ -2,14 +2,12 @@ from aqt import gui_hooks
 # import all of the Qt GUI library
 #from anki import hooks
 from anki.hooks import addHook
-import os, sys
+#import os, sys
 
-libfolder = os.path.dirname(__file__)
-sys.path.insert(0, libfolder)
-from card_template import add_reverso_notetype
-from language_management import setup_lang_buttons
-from focus_lost import onFocusLost
+#libfolder = os.path.dirname(__file__)
+#sys.path.insert(0, libfolder)
+from . import card_template, language_management, focus_lost
 
-addHook('editFocusLost', onFocusLost)
-gui_hooks.main_window_did_init.append(add_reverso_notetype)
-gui_hooks.editor_did_load_note.append(setup_lang_buttons)
+addHook('editFocusLost', focus_lost.onFocusLost)
+gui_hooks.main_window_did_init.append(card_template.add_reverso_notetype)
+gui_hooks.editor_did_load_note.append(language_management.setup_lang_buttons)
